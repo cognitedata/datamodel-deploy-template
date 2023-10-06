d73ced69fcc95167725072f156d8808b928c0095
anders-albert <anders0albert@gmail.com>
2023-10-06 07:38:50 +0200

    docs; Fixed some typoes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # datamodel-deploy-template
 Template repository for deploying data models to CDF
 
-Create an github environent (default name in the pipeline assumes `test`).
-In the environment add the following variables:
+Create an github environment (default name in the pipeline assumes `test`).
+In the environment, add the following variables:
 - `COGNITE_TENANT_ID`
 - `COGNITE_CLIENT_ID`
 - `COGNITE_PROJECT`
@@ -14,6 +14,11 @@ Add a secret named:
 If you are using non AAD idP like AUTH0 or Keycloak, set the environment variables `COGNITE_TOKEN_URL` and `COGNITE_AUDIENCE` as well (`COGNITE_TENANT_ID` not needed). `deploy-push.yaml` also needs modifications to use these variables (uncomment 4 lines)
 
 
-The `datamodels` folder contains one folder per datamodel. The name of the folder will be the name and externalid of the model. In the folder put a file named `datamodel.graphql` with the schema and a config file named `datamodel_config.yaml` where you can specify specific config for the model. Currently it's `version` and `space` to deploy to. You can also create a file named `datamodel_config_<environment>.yaml` to have environment specific configuration.
+The `datamodels` folder contains one folder per datamodel. The name of the folder will be the name and externalid 
+of the model. In the folder put a file named `datamodel.graphql` with the schema and a config file named 
+`datamodel_config.yaml` where you can specify specific config for the model. Currently, it's `version` and `space` 
+to deploy to. You can also create a file named `datamodel_config_<environment>.yaml` to have environment-specific configuration.
 
-In the `datamodel.graphql` you can use the variables `$SPACE` and `$VERSION`, these will be replace by the `space` and `version` specifed in the `datamodel_config.yaml`
+In the `datamodel.graphql` you can use the variables `$SPACE` and `$VERSION`, these will be replaced by the `space` 
+and `version` specifed in the `datamodel_config.yaml`
+S
--- build_scripts/changed_models.py
+++ build_scripts/changed_models.py
@@ -14,33 +14,31 @@ def parse_args():
         nargs=1,
         help="The folders to monitor for deployment of data models",
     )
-    parser.add_argument("deploy_all", type=str, nargs="?", help="The folders to deploy to all", default=None)
+    parser.add_argument("deploy_all", type=str, nargs="?", help="The folders/files to deploy to all", default=None)
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    datamodel_folders = sorted({f"models/{f.strip()}" for f in args.folders[0].split(",")})
-    print(f"Input: Folders to consider (re)deploying: {datamodel_folders}")
+    check_folders = Path(args.folders[0])
+    if not check_folders.exists():
+        print("Input: Folders to consider (re)deploying does not exist")
+        exit(1)
 
     deploy_all_folder = None
     if args.deploy_all:
-        deploy_all_folder = args.deploy_all.strip()
-        print(f"Input: Common folder (may force deploy all): {deploy_all_folder!r}")
+        deploy_all_folder = Path(args.deploy_all.strip())
+        print(f"Input: Common folder/file (may force deploy all): {deploy_all_folder!r}")
 
     # Compare against previous commit under the assumption of squash-only merges:
     diff = subprocess.check_output("git diff --name-only HEAD^ HEAD".split(), text=True).split()
     changed_files = set(map(Path, diff))
-    
-    deploy_all = False
-    if deploy_all_folder is not None:
-        deploy_all = any(f.is_relative_to(deploy_all_folder) for f in changed_files)
 
-    if deploy_all:
-        to_deploy = datamodel_folders
+    if deploy_all_folder is not None and any(f.is_relative_to(deploy_all_folder) for f in changed_files):
+        to_deploy = check_folders
         print("Common folder has one or more changed file(s), will deploy all functions")
     else:
-        to_deploy = [fld.replace("datamodels/","") for fld in datamodel_folders if any(f.is_relative_to(fld) for f in changed_files)]
+        to_deploy = [fld.replace("datamodels/","") for fld in check_folders if any(f.is_relative_to(fld) for f in changed_files)]
 
     if to_deploy:
         print(f"To be deployed: {to_deploy}")
@@ -48,9 +46,15 @@ def main():
         print("No changed folders detected, skipping deployment!")
         to_deploy = ["skipDeploy"]
 
-    with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
-        print(f"matrix={json.dumps({'folders': to_deploy})}", file=fh)
-        print(f"folders={to_deploy}", file=fh)
+    if "GITHUB_OUTPUT" not in os.environ:
+        # For local testing
+        print(f"matrix={json.dumps({'folders': str(to_deploy)})}")
+        print(f"folders={to_deploy}")
+        return
+    else:
+        with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
+            print(f"matrix={json.dumps({'folders': to_deploy})}", file=fh)
+            print(f"folders={to_deploy}", file=fh)
 
 
 if __name__ == "__main__":
