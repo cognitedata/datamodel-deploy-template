4ed6989a75762789f988abe155739aafa38718bf
anders-albert <anders0albert@gmail.com>
2023-10-06 07:56:42 +0200

    build: Finished changed models script

--- build_scripts/changed_models.py
+++ build_scripts/changed_models.py
@@ -2,8 +2,12 @@ import argparse
 import json
 import os
 import subprocess
-
+import itertools
 from pathlib import Path
+
+REPO_ROOT = Path(__file__).parent.parent
+
+CONFIG_FILE = (REPO_ROOT / "models" / "config.yaml").relative_to(REPO_ROOT)
 
 
 def parse_args():
@@ -14,31 +18,52 @@ def parse_args():
         nargs=1,
         help="The folders to monitor for deployment of data models",
     )
-    parser.add_argument("deploy_all", type=str, nargs="?", help="The folders/files to deploy to all", default=None)
+    parser.add_argument(
+        "deploy_all",
+        type=str,
+        nargs="?",
+        help="The folders/files to deploy to all",
+        default=None,
+    )
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    check_folders = Path(args.folders[0])
-    if not check_folders.exists():
-        print("Input: Folders to consider (re)deploying does not exist")
+    check_folders = [Path(f.strip()) for f in args.folders[0].split(",")]
+    if missing := [f for f in check_folders if not f.exists()]:
+        print(f"Input: Folders, {missing}, do not exist")
         exit(1)
 
-    deploy_all_folder = None
+    monitor_models = {
+        model
+        for folder in check_folders
+        for model in folder.glob("**/*.yaml")
+        if model != CONFIG_FILE
+    }
+    print("Found {len(monitor_models)} models to monitor for deployment")
+
+    deploy_all_folders = []
     if args.deploy_all:
-        deploy_all_folder = Path(args.deploy_all.strip())
-        print(f"Input: Common folder/file (may force deploy all): {deploy_all_folder!r}")
+        deploy_all_folders = [Path(f.strip()) for f in args.deploy_all.split(",")]
+        print(f"Input: Folder/file (may force deploy all): {deploy_all_folders!r}")
 
     # Compare against previous commit under the assumption of squash-only merges:
-    diff = subprocess.check_output("git diff --name-only HEAD^ HEAD".split(), text=True).split()
+    diff = subprocess.check_output(
+        "git diff --name-only HEAD^ HEAD".split(), text=True
+    ).split()
     changed_files = set(map(Path, diff))
 
-    if deploy_all_folder is not None and any(f.is_relative_to(deploy_all_folder) for f in changed_files):
-        to_deploy = check_folders
-        print("Common folder has one or more changed file(s), will deploy all functions")
+    if deploy_all_folders and any(
+        changed.is_relative_to(deploy_all)
+        for changed, deploy_all in itertools.product(changed_files, deploy_all_folders)
+    ):
+        to_deploy = monitor_models
+        print(
+            "Common folder has one or more changed file(s), will deploy all functions"
+        )
     else:
-        to_deploy = [fld.replace("datamodels/","") for fld in check_folders if any(f.is_relative_to(fld) for f in changed_files)]
+        to_deploy = [changed for changed in changed_files if changed in monitor_models]
 
     if to_deploy:
         print(f"To be deployed: {to_deploy}")
